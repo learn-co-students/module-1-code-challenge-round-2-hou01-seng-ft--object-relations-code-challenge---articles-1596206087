406f168207694b1334dba470a19acefa8da48ca2
bebomwonyi <bebomwonyi@gmail.com>
2020-07-31 11:49:47 -0500

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,25 @@
 class Author
-  attr_accessor :name
-
-
+  attr_reader :name
   def initialize(name)
     @name = name
-  
   end
-
-
+  def name
+    @name
+  end
+  def articles
+    a = Article.all.select {|b|b.author == self}
+    binding.pry
+  end
+  def magazines
+    a = Article.all.select {|b|b.author == self}
+    b = a.select {|b| b.magazine}
+    c = b.uniq
+    binding.pry
+  end
+  def add_article(magazine, title)
+    Article.new(self, magazine,title)
+  end
+  def topic_areas
+    binding.pry
+  end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,31 @@
 class Magazine
   attr_accessor :name, :category
-
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
-
-
+  def name
+    @name
+  end
+  def category
+    @category
+  end
+  def self.all
+    @@all
+  end
+  def contributors
+    a = Article.all.select {|b|b.magazine == self}
+    b = a.select {|b|b.author} 
+  end
+  def find_by_name(name)
+    binding.pry
+  end
+  def article_titles
+    binding.pry
+  end
+  def contributing_authors
+    binding.pry
+  end
 end
